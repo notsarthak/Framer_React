94dcbbf46d0452fbb747200613de087e7082589f
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-12-16 14:23:30 +0530

    The initial prop of motion elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,31 +4,10 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.div className="home container"
-      animate={{
-        rotateZ:180,
-        opacity: 0.2,
-        marginTop: 500
-      }}
-    >
-      <motion.h2 
-        animate={{ 
-          fontSize: 50, 
-          color: "#ff2994", 
-          x: -130, 
-          y: 30 
-        }}
-      >
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div className="home container" initial={{opacity: 0}} animate={{opacity: 1}}>
+      <h2>Welcome to Pizza Joint </h2>
       <Link to="/base">
-        <motion.button
-          animate={{
-            scale:1.5
-          }}
-        >
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
