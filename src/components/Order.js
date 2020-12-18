2162fd32e87494eb23570d29727a7910f9964555
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-12-18 18:33:17 +0530

    Animating components exiting the DOM

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 const containerVariants = {
   hidden: {
@@ -29,20 +29,15 @@ const childVariants = {
 };
 
 const Order = ({ pizza }) => {
-  const [showTitle, setShowTitle] = useState(true);
-  setTimeout(() => {
-    setShowTitle(false);
-  }, 3000);
   return (
     <motion.div
       className="container order"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
+      exit=""
     >
-      <AnimatePresence>
-        {showTitle && <motion.h2 exit={{y: "-100vh"}}>Thank you for your order :)</motion.h2>}
-      </AnimatePresence>
+      <h2>Thank you for your order :)</h2>
       <motion.p variants={childVariants}>
         You ordered a {pizza.base} pizza with:
       </motion.p>
