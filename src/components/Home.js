512dfc222cdb2edaaeec832decf3ed536bc07d17
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-12-21 20:19:26 +0530

    Created a loader using framer-motion

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import Loader from "./Loader";
+
 const buttonVariants = {
   hover: {
     scale: 1.1,
@@ -51,6 +53,7 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
+      <Loader/>
     </motion.div>
   );
 };
