6c1173bba258b7d19ff924845c6c44324f485b6e
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-12-22 11:25:39 +0530

    useCycle hook framer-motion

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
   animationOne: {
@@ -13,19 +13,36 @@ const loaderVariants = {
       y: {
         yoyo: Infinity,
         duration: 0.25,
-        ease: "easeOut"
+        ease: "easeOut",
+      },
+    },
+  },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+        ease: "easeOut",
       },
     },
   },
 };
 
 const Loader = () => {
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
   return (
-    <motion.div
-      className="loader"
-      variants={loaderVariants}
-      animate="animationOne"
-    ></motion.div>
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate={animation}
+      ></motion.div>
+      <div onClick={() => cycleAnimation()} className="change-loader">
+        Change Loader Animation
+      </div>
+    </>
   );
 };
 
